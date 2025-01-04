e31bf2c46437f67a55f84db03ab923a3a42db2ff
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-01-04 16:10:07 -0600

    :sparkles: feat: add directives and custom directives

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,19 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { User } from './models/user.model';
 import { randomInt } from 'crypto';
 import { GraphQLError } from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { countryCodeDirectiveTransformer, upperCaseDirectiveTransformer } from './directives/directives.js';
 
+
+
+/*
+    Directivas built-in
+
+@deprecated(reason:String)
+@skip(if:Boolean)
+@include(if:Boolean)
+
+*/
 
 
 // Origen de datos
@@ -13,8 +25,11 @@ const data = await fetchData(config.api.url)
 const users = await fetchUser()
 
 // Definición de tipos
-
 const typeDefs = `
+    # Definition of directives
+    directive @uppercase on FIELD_DEFINITION
+    directive @countryCode on FIELD_DEFINITION
+
     type Rating {
         rate: Int
         count: Int
@@ -32,20 +47,22 @@ const typeDefs = `
 
     type User {
         id: ID!
-        name: String!
+        name: String! @uppercase
         surname: String!
-        street: String!
+        street: String
         zipCode: Int!
         city: String!
-        phone: String
+        phone: String @countryCode
         address: String
+        fullName: String 
     }
 
     type Query {
         products: [Product]
         product(id:ID!): Product
         users: [User]
-        userCount: Int!
+        userCount: Int! @deprecated(reason: "Use usersLength instead")
+        userLength: Int!
         findUserByName(name: String!): User
         findUserById(id: ID!): User
     }
@@ -57,24 +74,24 @@ const typeDefs = `
             street: String!, 
             zipCode: Int!, 
             city: String!, 
-            phone: String
+            phone: String!
         ): User
     }
 ` 
-
-
 
 // Solucionadores
 
 const resolvers = {
     User: {
-        address: (parent: any) => `${parent.street}, ${parent.zipCode}, ${parent.city}`
+        address: (parent: any) => `${parent.street}, ${parent.zipCode}, ${parent.city}`,
+        fullName: (parent: any) => `${parent.name} ${parent.surname}`,
     },
     Query: {
         products: () => data,
         product: (_:any, args:{ id: string }) => data.find((product: any) => product.id === parseInt(args.id)),
         users: () => users,
         userCount: () => users.length,
+        userLength: () => users.length,
         findUserByName: (_:any, args:{ name: string }) => users.find((user: User) => user.name === args.name),
         findUserById: (_:any, args: { id: string}) => users.find((user: User) => user.id === parseInt(args.id))
     },
@@ -98,25 +115,20 @@ const resolvers = {
 
 }
 
-// Mutaciones
-
-// Suscripciones
-
-
-
-//Servidor
-
-const server = new ApolloServer({
+// Schema
+let schema = makeExecutableSchema({
     typeDefs,
     resolvers
 })
 
+// Add custom directives
+schema = upperCaseDirectiveTransformer(schema, 'uppercase');
+schema = countryCodeDirectiveTransformer(schema, 'countryCode');
+
+const server = new ApolloServer({
+    schema
+})
+
 
 const { url } = await startStandaloneServer(server, {listen: {port: 4000}})
-
-
-
-
 console.log(`Server ready at ${url}`)
-
-
